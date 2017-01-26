8786d903fbb777084aa5d6783b7445155d497db0
jmcshane <jmcshan1@gmail.com>
2017-01-26 16:20:39 -0600

    Removing promotion

--- pipeline.groovy
+++ pipeline.groovy
@@ -32,20 +32,20 @@ node('maven') {
   	sh "${OC_CMD} start-build ${env.APP_NAME} --from-file=${file} --wait=true --follow=true"
   }
 
-  input "Promote to QA?"
+  // input "Promote to QA?"
 
-  stage('Promote to QA') {
-    sh """
-    ${env.OC_CMD} tag ${env.STAGE1}/${env.APP_NAME}:latest ${env.STAGE2}/${env.APP_NAME}:latest
-    """
+  // stage('Promote to QA') {
+  //   sh """
+  //   ${env.OC_CMD} tag ${env.STAGE1}/${env.APP_NAME}:latest ${env.STAGE2}/${env.APP_NAME}:latest
+  //   """
 
-    input "Promote Application to Prod?"	
-  }
+  //   input "Promote Application to Prod?"	
+  // }
 
-  stage('Promote To Prod') {
-    sh """
-    ${env.OC_CMD} tag ${env.STAGE1}/${env.APP_NAME}:latest ${env.STAGE3}/${env.APP_NAME}:latest
-    """
+  // stage('Promote To Prod') {
+  //   sh """
+  //   ${env.OC_CMD} tag ${env.STAGE1}/${env.APP_NAME}:latest ${env.STAGE3}/${env.APP_NAME}:latest
+  //   """
 
-  }
+  // }
 }
